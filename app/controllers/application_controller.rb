437521b034d4b42447db1e7b0cf95ba6c354c820
Chris Doyle <cdoyle@temple.edu>
2020-02-20 15:28:15 -0500

    Update menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   helper_method :logged_in?
   helper_method :is_admin?
+  helper_method :is_student?
+  helper_method :bulletin_user?
 
   def current_user
     User.find_by(id: session[:user_id])
@@ -12,6 +14,14 @@ class ApplicationController < ActionController::Base
 
   def is_admin?
     current_user.username == "admin"
+  end
+
+  def is_student?
+    current_user.username == "student"
+  end
+
+  def bulletin_user?
+    current_user.username == "bulletin"
   end
 
   def logged_in?
