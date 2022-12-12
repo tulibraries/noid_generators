56e4176dcaa31637861558ad1f5849755fc03956
sensei100 <tuh42082@temple.edu>
2022-12-12 15:07:28 -0500

    Try adding config.active_support.bare = true

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module NoidGenerators
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.active_support.bare = true
 
     # Configuration for the application, engines, and railties goes here.
     #
